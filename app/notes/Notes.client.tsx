af6013a50d29fe12b31cbb7fcff91a759ad1ac07
unknown <Fqva@ukr.net>
2026-08-30 11:24:11 +0300

    add carcas conf

---
+++ app/notes/Notes.client.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import css from "./Notes.module.css";
+import { useEffect, useState } from "react";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
+import { useDebouncedCallback } from "use-debounce";
+
+import { fetchNotes } from "@/lib/api/api";
+
+import Modal from "../../components/Modal/Modal";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import NoteList from "../../components/NoteList/NoteList";
+import NoteForm from "../../components/NoteForm/NoteForm";
+import Pagination from "../../components/Pagination/Pagination";
+import Loader from "../../components/Loader/Loader";
+
+export default function NotesClient() {
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isOpenModal, setisOpenModal] = useState(false);
+
+  const {
+    data: notes,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["note", search, currentPage],
+    queryFn: () => fetchNotes(search, currentPage),
+    retry: 1,
+    staleTime: 5000,
+    placeholderData: keepPreviousData,
+    refetchOnMount: false,
+  });
+  //   console.log(notes?.totalPages);
+  const handleClick = () => {
+    setisOpenModal(!isOpenModal);
+  };
+  const updateSearchQuery = useDebouncedCallback((value: string) => {
+    setSearch(value);
+    setCurrentPage(1);
+  }, 500);
+  useEffect(() => {
+    if (isError || notes?.notes.length === 0) {
+      toast("Failed to load notes or no matches found. Please try again.");
+    }
+  }, [isError, notes]);
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {<SearchBox updateSearchQuery={updateSearchQuery} />}
+
+        {notes && notes.notes.length > 0 && (notes.totalPages ?? 1) > 1 && (
+          <Pagination
+            totalPages={notes.totalPages ?? 1}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
+        )}
+
+        {
+          <button className={css.button} onClick={handleClick}>
+            Create note +
+          </button>
+        }
+      </header>
+      {isLoading && <Loader />}
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            border: "1px solid #713200",
+            background: "#d67719cb",
+          },
+        }}
+      />
+      {isOpenModal && (
+        <Modal onClose={handleClick} isOpen={isOpenModal}>
+          <NoteForm onClose={handleClick} />
+        </Modal>
+      )}
+
+      {isSuccess && notes && <NoteList notes={notes?.notes} />}
+    </div>
+  );
+}
---
+++ lib/api.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { Note } from "@/types/note";
+
+// Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkZxdmFAdWtyLm5ldCIsImlhdCI6MTc4NjYyMjc0Mn0.jkg9S2Kty2N0FrvCg1GBSW9zCjuWvjxmxCLSEkC-ik8";
+
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+
+interface NotesResponse {
+  notes: Note[];
+  totalPages?: number;
+}
+
+export const fetchNotes = async (
+  search?: string,
+  currentPage?: number,
+): Promise<NotesResponse> => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      search: search,
+      page: currentPage,
+    },
+  };
+
+  const resp = await axios.get<NotesResponse>("/notes", config);
+  return resp.data;
+};
+
+export const fetchNoteById = async (id: Note["id"]): Promise<Note> => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data } = await axios.get(`/notes/${id}`, config);
+  return data;
+};
+
+export const addNote = async (
+  noteData: Pick<Note, "title" | "content" | "tag">,
+): Promise<Note> => {
+  const config = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data } = await axios.post<Note>("/notes", noteData, config);
+  return data;
+};
+
+export const deleteNote = async (id: Note["id"]): Promise<Note> => {
+  const { data } = await axios.delete<Note>(`/notes/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return data;
+};
